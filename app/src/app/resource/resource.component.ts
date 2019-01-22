3f6d9878aab03de37617edcb67f3c6418663a5c4
André Kilchenmann <github@milchkannen.ch>
2019-01-22 15:54:34 +0100

    enhancement (resource): show incoming links

--- app/src/app/resource/resource.component.ts
+++ app/src/app/resource/resource.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import {
-    ApiServiceError, ImageRegion,
+    ApiServiceError, ImageRegion, IncomingService,
     KnoraConstants,
     OntologyInformation, ReadResource,
     ReadResourcesSequence, ReadStillImageFileValue,
@@ -26,7 +26,8 @@ export class ResourceComponent implements OnInit {
 
     constructor(protected _route: ActivatedRoute,
                 protected _router: Router,
-                protected _resourceService: ResourceService
+                protected _resourceService: ResourceService,
+                protected _incomingService: IncomingService
     ) {
         this._route.params.subscribe((params: Params) => {
             this.iri = params['iri'];
@@ -40,7 +41,11 @@ export class ResourceComponent implements OnInit {
                 console.log(result);
                 this.resource = result;
 
+                // collect images and regions
                 this.collectImagesAndRegionsForResource(this.resource.resources[0]);
+
+                // get incoming resources
+                this.requestIncomingResources();
 
 
                 // wait until the resource is ready
@@ -123,6 +128,87 @@ export class ResourceComponent implements OnInit {
 
         resource.stillImageRepresentationsToDisplay = imgRepresentations;
 
+    }
+
+    requestIncomingResources(): void {
+
+        // make sure that this.resource has been initialized correctly
+        if (this.resource === undefined) {
+            return;
+        }
+
+        // request incoming regions
+        if (this.resource.resources[0].properties[KnoraConstants.hasStillImageFileValue]) {
+            // TODO: check if resources is a StillImageRepresentation using the ontology responder (support for subclass relations required)
+            // the resource is a StillImageRepresentation, check if there are regions pointing to it
+
+            this.getIncomingRegions(0);
+
+        } else {
+            // this resource is not a StillImageRepresentation
+            // check if there are StillImageRepresentations pointing to this resource
+
+            // this gets the first page of incoming StillImageRepresentations
+            // more pages may be requested by [[this.viewer]].
+            // TODO: for now, we begin with offset 0. This may have to be changed later (beginning somewhere in a collection)
+            // this.getIncomingStillImageRepresentations(0);
+        }
+
+        // check for incoming links for the current resource
+        this.getIncomingLinks(0);
+
+
+    }
+
+    getIncomingRegions(offset: number, callback?: (numberOfResources: number) => void): void {
+        this._incomingService.getIncomingRegions(this.resource.resources[0].id, offset).subscribe(
+            (regions: ReadResourcesSequence) => {
+                // update ontology information
+                this.ontologyInfo.updateOntologyInformation(regions.ontologyInformation);
+
+                // Append elements of regions.resources to resource.incoming
+                Array.prototype.push.apply(this.resource.resources[0].incomingRegions, regions.resources);
+
+                // prepare regions to be displayed
+                this.collectImagesAndRegionsForResource(this.resource.resources[0]);
+
+                // TODO: implement osdViewer
+                /* if (this.osdViewer) {
+                  this.osdViewer.updateRegions();
+                } */
+
+                // if callback is given, execute function with the amount of new images as the parameter
+                if (callback !== undefined) {
+                    callback(regions.resources.length);
+                }
+            },
+            (error: any) => {
+                console.error(error);
+                this.loading = false;
+            }
+        );
+    }
+
+    getIncomingLinks(offset: number, callback?: (numberOfResources: number) => void): void {
+
+        this._incomingService.getIncomingLinksForResource(this.resource.resources[0].id, offset).subscribe(
+            (incomingResources: ReadResourcesSequence) => {
+                // update ontology information
+                this.ontologyInfo.updateOntologyInformation(incomingResources.ontologyInformation);
+
+                // Append elements incomingResources to this.resource.incomingLinks
+                Array.prototype.push.apply(this.resource.resources[0].incomingLinks, incomingResources.resources);
+
+                // if callback is given, execute function with the amount of incoming resources as the parameter
+                if (callback !== undefined) {
+                    callback(incomingResources.resources.length);
+                }
+            },
+            (error: any) => {
+                console.error(error);
+                this.loading = false;
+            }
+        );
     }
 
     openLink(prop: any) {
